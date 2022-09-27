532d671c499ba1139d91280101c69ab3b6ec65a9
merimaceranic98 <merima.ceranic@ministryofprogramming.com>
2022-09-27 11:21:54 +0200

    Adding Chakra UI, and Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+
+import { ROUTES } from "./routing/routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Routes>
+        {ROUTES.map((route) => {
+          return (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.element />}
+            />
+          );
+        })}
+      </Routes>
+    </ChakraProvider>
   );
 }
 
---
+++ src/routing/routes.ts
@@ -0,0 +1,9 @@
+import Home from "../features/home/components/Home";
+
+const ROUTES = [
+  {
+    path: "/",
+    element: Home,
+  },
+];
+export { ROUTES };
