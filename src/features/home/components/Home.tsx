b5c97025b06f91df5abad4f93472b32cb2423476
merimaceranic98 <merima.ceranic@ministryofprogramming.com>
2022-10-11 17:08:24 +0200

    Adding typewriter effect, and updating home page

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -1,6 +1,40 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Center, Container, Flex, Text } from "@chakra-ui/react";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
-  return <Container mt={"3rem"}>Home component</Container>;
+  return (
+    <Container mt={"3rem"} maxW={"auto"}>
+      <Center display={"flex"} flexDirection={"column"} mt={"200px"}>
+        <Flex alignItems={"center"} mb={10}>
+          <Box fontSize={"24px"} mr={"3px"}>
+            Build your products with
+          </Box>
+          <Box fontWeight={"bold"} fontSize={"24px"} color={"red.500"}>
+            CompanySY
+          </Box>
+        </Flex>
+        <Flex flexDirection={"row"} display={"flex"}>
+          <Text mr={"3px"} fontSize={"32px"}>
+            We are CompanySY with millions of
+          </Text>
+          <Text fontSize={"32px"}>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("ideas.")
+                  .pauseFor(200)
+                  .deleteAll()
+                  .typeString("solutions.")
+                  .pauseFor(200)
+                  .deleteAll()
+                  .typeString("stories.")
+                  .start();
+              }}
+            />
+          </Text>
+        </Flex>
+      </Center>
+    </Container>
+  );
 };
 export default Home;
